b8184fb60948d41e8f6b658c7a561acebe1bfbd1
WooKyoung Noh <wookay.noh@gmail.com>
2016-04-06 18:48:04 +0900

    TableSections

--- controllers/TestTableSections.swift
+++ controllers/TestTableSections.swift
@@ -8,24 +8,54 @@
 
 import UIKit
 
+class OrganizeSettings: TableSectionItem {
+    var title: String
+    var rows: [TableRowItem]
+    init() {
+        self.title = ""
+        self.rows = []
+    }
+    func cellForRowAtIndexPath(vc: UIViewController, _ cell: UITableViewCell?, indexPath: NSIndexPath) {
+    }
+    func didSelectRowAtIndexPath(vc: UIViewController, indexPath: NSIndexPath) {
+    }
+}
 
-class TestTableSections: WTestCase {
-    class OrganizeSettings: TableSectionItem {
-        var title: String
-        var rows: [TableRowItem]
-        init() {
-            self.title = ""
-            self.rows = []
-        }
-        func cellForRowAtIndexPath(vc: UIViewController, _ cell: UITableViewCell?, indexPath: NSIndexPath) {
-        }
-        func didSelectRowAtIndexPath(vc: UIViewController, indexPath: NSIndexPath) {
-        }
+
+
+class TableSectionViewController: UITableViewController {
+    var ts: TableSections? = TableSections(sections:
+        [OrganizeSettings(), OrganizeUpgrade(), OrganizeAppInfo()])
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return ts!.sections.count
     }
     
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ts!.sections[section].rows.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)
+        if nil == cell {
+            cell = UITableViewCell(style: .Value1, reuseIdentifier: cellIdentifier)
+        }
+        ts!.sectionitemof(indexPath).cellForRowAtIndexPath(self, cell, indexPath: indexPath)
+        return cell!
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        ts!.sectionitemof(indexPath).didSelectRowAtIndexPath(self, indexPath: indexPath)
+    }
+}
+
+
+
+class TestTableSections: WTestCase {
+    
     func test_tablesections() {
-        let ts = TableSections(sections:
-            [OrganizeSettings(), OrganizeUpgrade(), OrganizeAppInfo()])
-        Assert.equal(3, ts.sections.count)
+        let vc = TableSectionViewController()
+        Assert.equal(3, vc.ts!.sections.count)
     }
 }
